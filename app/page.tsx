7096bd03e9f20e65853fc9690a01eaa3d239052d
Nelson Cadenas <nca1478@yahoo.com>
2024-10-18 14:32:10 -0400

    23. Resumen de la sección

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex flex-col items-center p-24">
       <span className="text-5xl">Home Page</span>
+
+      <Link className="pt-2" href={"/about"}>
+        About Page
+      </Link>
     </main>
   );
 }
