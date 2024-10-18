fc49edf69f510da19f517b91ff0cd6e15ea17852
Nelson Cadenas <nca1478@yahoo.com>
2024-10-18 13:20:49 -0400

    20. Next/Link

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,19 +1,36 @@
+import { HomeIcon } from "@primer/octicons-react";
+import Link from "next/link";
+
+const navItems = [
+  {
+    path: "/about",
+    text: "About",
+  },
+  {
+    path: "/pricing",
+    text: "Pricing",
+  },
+  {
+    path: "/contact",
+    text: "Contact",
+  },
+];
+
 export const Navbar = () => {
   return (
     <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-      <span>Home</span>
+      <Link href={"/"} className="flex items-center">
+        <HomeIcon className="mr-2" />
+        <span>Home</span>
+      </Link>
 
       <div className="flex flex-1"></div>
 
-      <a href="/about" className="mr-2">
-        About
-      </a>
-      <a href="/pricing" className="mr-2">
-        Pricing
-      </a>
-      <a href="/contact" className="mr-2">
-        Contact
-      </a>
+      {navItems.map((navItem) => (
+        <Link key={navItem.text} href={navItem.path} className="mr-2">
+          {navItem.text}
+        </Link>
+      ))}
     </nav>
   );
 };
